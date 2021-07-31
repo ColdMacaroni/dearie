a694c1ffe1a9c2f61901dc5cbc5aa60d03394903
muffin <squishysparrow7.7@gmail.com>
2021-07-31 21:23:01 +1200

    Print config in main (DEBUG)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #include "readconfig.h"
 
 int
 main(int argc, char **argv)
 {
-    
+    config_t config = readConfig(NULL);
+    printf("Entries dir: %s\n12hr format: %s\nTemplate: %s\n",
+            config.entries_dir,
+            (config.twelve_hours) ? "true" : "false",
+            config.template_str);
 }
