87cd8d8a6a567dd50b48378ddb1e0291adcbd4f8
muffin <squishysparrow7.7@gmail.com>
2021-07-31 21:56:43 +1200

    starting on arg reading

--- src/main.c
+++ src/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
 #include "readconfig.h"
 
 int
 main(int argc, char **argv)
 {
-    config_t config = readConfig(NULL);
+    char config_path[BUFSIZ];
+    sprintf(config_path, "%s/.config/PGM_NAME/")
+
+    // Go through arguments and stuff
+    {
+        char *current_arg;
+        for (int arg = 0; arv < argc; arg++)
+        {
+            if (argv[arg][0] == '-')
+        }
+    }
+
+    config_t config = readConfig(config_path);
     printf("Entries dir: %s\n12hr format: %s\nTemplate: %s\n",
             config.entries_dir,
             (config.twelve_hours) ? "true" : "false",
